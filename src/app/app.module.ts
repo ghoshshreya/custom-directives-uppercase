f16152e95a0caf5a54d4b6b44975dbc907b38af7
ghoshshreya <shrghosh08@gmail.com>
2023-04-11 21:48:50 +0530

    2 files modified, 1 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { UppercaseDirective } from './uppercase.directive';
 
 @NgModule({
   imports: [BrowserModule, FormsModule],
-  declarations: [AppComponent, HelloComponent, UppercaseDirective],
+  declarations: [AppComponent, UppercaseDirective],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
